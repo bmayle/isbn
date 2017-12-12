a07807cae6abb2f08ff9e13bcc479857b7817902
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-12 03:38:03 -0500

    Everything works for 10 and 13 ISBN

--- isbn.rb
+++ isbn.rb
@@ -1,14 +1,14 @@
-# def isbn(isbn_num)
-# 	isbn_num.class
-# 	if isbn_num.class == Integer
-# 		Integer
-# 	else
-# 		false
-# 	end
-# end
+def isbn(isbn_num)
+	isbn_num.class
+	if isbn_num.class == Integer
+		Integer
+	else
+		false
+	end
+end
 
 def check_isbn(isbn_num)
-    isbn_arr = isbn_num.to_s.split(''){s.to_i}
+    isbn_arr = isbn_num.to_s.split('')
     if isbn_arr.length == 10 || isbn_arr.length == 13
         true
     else
@@ -16,54 +16,53 @@ def check_isbn(isbn_num)
 	end
 end
 
-def isbn_valid_13(isbn_num)
-	isbn_number = isbn_num.to_s.split(''){s.to_i}
-
-	isbn_length = isbn_number.count	
-	counter = 0
-	sum = 0
-	multiply = 0
-
-	isbn_length.times do
-		isbn_number[counter] = isbn_number[counter].to_i
-
-		if counter != 12
-			if counter % 2 == 0
-				sum = sum + isbn_number[counter]
-			else
-				sum = sum + isbn_number[counter] * 3
-			end
-
-			counter = counter - 1
-		end
-	end
-
-	if multiply = sum % 10
-	checker = 10 - multiply
-	if checker == isbn_number[12]
-		true
-	else
-		false
-	end
-end
-
-def isbn_valid_10(isbn_num)
-	isbn_number = isbn_num.to_s.split(''){s.to_i}
+def validate_isbn_10(isbn_num)
+	ten_isbn = isbn_num.to_s.split('')
 	
-	isbn_length = isbn_number.count	
+	isbn_length = ten_isbn.count	
 	counter = 0
 	sum = 0
-	multiply = 10
+	input = 10
 
 	isbn_length.times do
-		sum = sum + (isbn_number[counter].to_i * multiply)
+		sum = sum + ten_isbn[counter].to_i * input
 		counter = counter + 1
-		multiply = multiply - 1
+		input = input - 1
 	end
 
 	if sum % 11 == 0 
 		true
 	else
 		false	
+	end
+end
+
+def validate_isbn_13(isbn_num)
+	thirteen_isbn = isbn_num.to_s.split('')
+	
+	isbn_length = thirteen_isbn.count	
+	counter = 0
+	sum = 0
+	input = 0
+
+	isbn_length.times do
+		thirteen_isbn[counter] = thirteen_isbn[counter].to_i 
+		if counter != 12
+			if counter % 2 == 0 
+				sum = sum + counter * 3
+			elsif 
+				sum = sum + counter
+			end
+			counter = counter + 1
+		end
+	end
+	input = sum % 10 
+	input = 10 - input
+
+
+	if input == thirteen_isbn[12]
+		true
+	else
+		false
 	end
 end
--- isbn_test.rb
+++ isbn_test.rb
@@ -6,15 +6,15 @@ class TestISBN < Minitest::Test
 		assert_equal(1, 1)
 	end
 
-	# def test_assert_that_isbn_num_is_integer
- #    	assert_equal(Integer, isbn(7))
- #    end
+	def test_assert_that_isbn_num_is_integer
+    	assert_equal(Integer, isbn(7))
+    end
 
- #    def test_assert_that_wombat_is_not_a_integer
- #    	assert_equal(false, isbn("wombat"))
- #    end
+    def test_assert_that_wombat_is_not_a_integer
+    	assert_equal(false, isbn("wombat"))
+    end
 
-	def test_that_false_is_returned_in_check_isbn_function_if_less_than_10
+	def test_that_false_is_returned_if_length_less_than_10
         isbn_num = 147852963
 		assert_equal(false, check_isbn(isbn_num))
     end
@@ -24,23 +24,33 @@ class TestISBN < Minitest::Test
 		assert_equal(true, check_isbn(isbn_num))
     end
 
+    def test_that_false_is_returned_if_length_less_than_13
+        isbn_num = 147852968888
+		assert_equal(false, check_isbn(isbn_num))
+	end
+	
     def test_that_true_is_returned_if_length_returns_13
     	isbn_num = 9876543210369
     	assert_equal(true, check_isbn(isbn_num))
     end
 
     def test_that_isbn_invalid_for_10_digits
-    	isbn_num = 7896541230
-    	assert_equal(false, isbn_valid_10(isbn_num))
+    	isbn_num = 7465823190
+    	assert_equal(false, validate_isbn_10(isbn_num))
     end
 
     def test_that_isbn_valid_for_10_digits
     	isbn_num = 9604250590
-    	assert_equal(true, isbn_valid_10(isbn_num))
+    	assert_equal(true, validate_isbn_10(isbn_num))
     end
 
-    def test_that_isbn_invalid_for_13_digits
-    	isbn_num = 7410258963012
-    	assert_equal(false, isbn_valid_13(isbn_num))
-    end
+    def test_for_isbn_invalid_for_13_figits
+		isbn_num = 1234567891011
+		assert_equal(false, validate_isbn_13(isbn_num))
+	end
+
+	def test_for_isbn_valid_for_13_figits
+		isbn_num = 9781566199094 
+		assert_equal(true, validate_isbn_13(isbn_num))
+	end
 end
